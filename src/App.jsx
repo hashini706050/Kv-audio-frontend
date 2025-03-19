00a4cf5e4b2936fdf9f51a73b26a06f1deef00cf
HashiniJ <it22171924@my.sliit.lk>
2025-03-19 09:42:07 +0530

    day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import'./App.css'
-import ProductCard from './components/productCard'
+import'./App.css';
+import ProductCard from './components/productCard';
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineReviews } from "react-icons/md";
 
 function App() {
 
   return (
-    <div>
-      <div className='w-11 h-11 bg-black fixed right-3 bottom-5'>
+    <div className='w-full h-screen flex'>
+      <div className="w-[300px] h-full bg-green-300">
+        <button className="w-full h-[40px] text-[25px] font-bold bg-red-400 flex justify-center items-center">
+          <BsGraphDown/>
+          Dashboard
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegBookmark/>
+          Bookings
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <MdOutlineLocalGroceryStore/>
+          Items
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegUser/>
+          Users
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <MdOutlineReviews/>
+          Reviews
+        </button>
+
       </div>
+      <div className="flex-1 h-full bg-red-900 relative">
+      
+
+      </div>
+      
     </div>
   )
 }
