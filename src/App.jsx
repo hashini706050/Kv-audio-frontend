e5b8f1988689ae036374fd54a6eb548d652a38df
HashiniJ <it22171924@my.sliit.lk>
2025-03-19 06:59:21 +0530

    import tailwind.css to project👍

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import './App.css'
+import'./App.css'
 import ProductCard from './components/productCard'
 
 function App() {
 
   return (
-    <>
-      <ProductCard name="Audio Setup" price="3250/=" description="Auto Connect Bluetooth." img="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg"/>
-      <ProductCard name="Aloe Vera Smoothing Gel" price="1890/=" description="our daily lip makeup requirements." img="https://img.drz.lazcdn.com/static/lk/p/649052204105e7582321e346016cb419.jpg_720x720q80.jpg"/>
-    </>
+    <div>
+      <div className='w-11 h-11 bg-black fixed right-3 bottom-5'>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
